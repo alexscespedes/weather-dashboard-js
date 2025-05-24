b2aab52b7cf70902836c321139b8e9eb5b213cea
alexscespedes <alexandersenc99@gmail.com>
2025-05-24 13:32:50 -0400

    Add API key logic with secret

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// OpenWeather API Key
-const API_KEY = "";
-
 // Get DOM Elements
 const form = document.getElementById("weather-form");
 const city = document.getElementById("city-input");
@@ -13,3 +10,23 @@ const description = document.getElementById("description");
 const temperature = document.getElementById("temperature");
 const humidity = document.getElementById("humidity");
 const windSpeed = document.getElementById("wind-speed");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const city = cityInput.value.trim();
+  if (!city) return;
+
+  errorMessage.classList.add("hidden");
+  weatherResult.classList.add("hidden");
+
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/weather?q=${encodeURIComponent(
+        city
+      )}&appid=${API_KEY}&units=metric`
+    );
+  } catch (error) {}
+});
+
+console.log(API_KEY);
