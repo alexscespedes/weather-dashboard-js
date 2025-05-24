2475109f1d45e65e7245ee6f62b58bb21c2f3ced
alexscespedes <alexandersenc99@gmail.com>
2025-05-24 13:14:13 -0400

    Add DOM elements in js

---
+++ app.js
@@ -0,0 +1,15 @@
+// OpenWeather API Key
+const API_KEY = "";
+
+// Get DOM Elements
+const form = document.getElementById("weather-form");
+const city = document.getElementById("city-input");
+const errorMessage = document.getElementById("error-message");
+const weatherResult = document.getElementById("weather-result");
+
+const cityName = document.getElementById("city-name");
+const weatherIcon = document.getElementById("weather-icon");
+const description = document.getElementById("description");
+const temperature = document.getElementById("temperature");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("wind-speed");
